8c349aba76242bfc0f69916a3fd872c7f2a72655
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2021-08-31 19:47:44 -0500

    Currently working on the ExcelMethods

--- ExcelProject/src/main/java/Methods/ExcelMethods.java
+++ ExcelProject/src/main/java/Methods/ExcelMethods.java
@@ -3,14 +3,24 @@ package Methods;
 import ExcelObjects.ExcelFile;
 import Inputs.ExcelInputs;
 import Model.User;
+import ObjectManager.ExcelManager;
 
 public class ExcelMethods extends ExcelInputs {
 
-    public ExcelFile createNewFile(User user){
-        System.out.println("*** Create Excel ***");
-        String fileName = setName2(user.getExcelManager());
+    public ExcelFile createNewFile(ExcelManager eManager){
+        System.out.println("*** Create Excel File ***");
+        String fileName = setName2(eManager);
         ExcelFile newFile = new ExcelFile(fileName);
-        user.getExcelManager().add(newFile);
+        eManager.add(newFile);
         return newFile;
+    }
+
+    public void removeFile(ExcelManager eManager){
+        System.out.println("*** Remove Excel File ***");
+        String fileName = setName();
+        boolean deleted = eManager.remove(fileName);
+        String message = deleted ? fileName + " Has Been Deleted From Database" :
+                fileName + " Could Not Be Found In Database";
+        System.out.println(message + "\n");
     }
 }
